0b6159ad681de005dcc462486593f09ce49839ed
Percian Joseph Borja <percian0296@gmail.com>
2023-01-17 13:46:34 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role',['admin','guard']);
+            $table->enum('status',['active','inactive'])->default('active');
             $table->string('rfid')->nullable();
             $table->rememberToken();
             $table->timestamps();
