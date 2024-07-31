9189c6f976305f9af74ced8530e5c37609d4049f
John Carlo Banzuela <banzuela.johncarlo@epcst.edu.ph>
2024-07-31 12:39:49 +0800

    IDSYS-001 New Roles
    Adding new roles to Users of EPCST ID System

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['admin','guard','user'])->default('user');
+            $table->enum('role',['admin','guard','student','saso','hr','employee'])->default('user');
             $table->enum('status',['active','inactive'])->default('active');
             $table->string('rfid')->nullable();
             $table->rememberToken();
